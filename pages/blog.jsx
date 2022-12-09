6934c8164193ba6e17d033fcfaad1ecd9e9de61c
gerritt-luoma <gerritt.luoma@gmail.com>
2022-12-08 17:51:51 -0700

    Started working on blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,6 +1,35 @@
+import ReactMarkdown from 'react-markdown';
 
-export default function Home() {
+const Blog = () => {
+    const test = `
+    # H1
+
+    ## H2
+
+    ### H3
+
+    Some regular text
+
+    Unordered list:
+    - item 1
+    - item 2
+    - item 3
+
+    Ordered list:
+    1. item 1
+    2. item 2
+    3. item 3
+
+    \`\`\`
+    # code block
+    def yeet():
+        return \'yeet\'
+    \`\`\`
+    `;
     return (
-      <div>Blog</div>
+        <ReactMarkdown children={test}/>
     )
-  }
+}
+
+
+export default Blog;
