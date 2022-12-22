2fa5641ad89ebb96123639341425d7db33cab2f8
gerritt-luoma <gerritt.luoma@gmail.com>
2022-12-22 11:13:49 -0700

    Added in mantine for styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
-import Navbar from '@components/Navbar/Navbar'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
   return (
     <>
-      <Navbar/>
-      <Component {...pageProps} />
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 }
