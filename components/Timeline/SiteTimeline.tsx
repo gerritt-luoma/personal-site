18d73a8ae9e4d192d86cbb7d7fe550dd122384da
gerritt-luoma <gerritt.luoma@gmail.com>
2022-12-26 09:06:59 -0700

    Fixed timeline styling. Can't have sep component

--- components/Timeline/SiteTimeline.tsx
+++ components/Timeline/SiteTimeline.tsx
@@ -1,15 +1,53 @@
-import { Timeline } from "@mantine/core";
+import { Timeline, Text } from "@mantine/core";
 import { TimelineItem as TimelineItemData } from "@prisma/client";
-import TimelineItem from "./TimelineItem";
+import { IconPlus, IconRefresh, IconAlertTriangle, IconDatabase, IconUserCheck, IconTrashX, IconQuestionCircle, TablerIcon } from "@tabler/icons";
+import { ItemType } from "@lib/enums";
 
 type TimelineProps = {
     items: TimelineItemData[]
 }
 
 const SiteTimeline = ({ items }: TimelineProps) => {
+    function determineIcon(type: number) {
+        let Icon: TablerIcon;
+        switch(type) {
+            case ItemType.ADDITION:
+                Icon = IconPlus;
+                break;
+            case ItemType.REMOVAL:
+                Icon = IconTrashX;
+                break;
+            case ItemType.DECISION:
+                Icon = IconAlertTriangle;
+                break;
+            case ItemType.FRONTEND_RELATED:
+                Icon = IconUserCheck;
+                break;
+            case ItemType.BACKEND_RELATED:
+                Icon = IconDatabase;
+                break;
+            case ItemType.REWORK:
+                Icon = IconRefresh;
+                break;
+            default:
+                console.error(`Invalid item type: ${type}`);
+                Icon = IconQuestionCircle;
+        }
+        return Icon;
+    }
     return (
-        <Timeline bulletSize={20} lineWidth={2} color={'pink.6'} m={'auto'}>
-            {items.map(item => <TimelineItem {...item}/>)}
+        <Timeline bulletSize={24} lineWidth={2} color={'pink.6'} active={1}>
+            {items.map(item => {
+                let Icon = determineIcon(item.type);
+                return (
+                    <Timeline.Item title={item.title} bullet={<Icon size={16}/>}>
+                        <Text color={'dimmed'} size={'xs'}>
+                            {item.blurb}:
+                            <Text color={'pink.6'}>{item.createdOn.toDateString()}</Text>
+                        </Text>
+                    </Timeline.Item>
+                );
+            })}
         </Timeline>
     );
 }
