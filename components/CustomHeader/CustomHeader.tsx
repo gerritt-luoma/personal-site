d1648a45c57e9c92f9f80e39b20dbea3195d7a42
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-04 16:47:59 -0700

    Adding responsive header

--- components/CustomHeader/CustomHeader.tsx
+++ components/CustomHeader/CustomHeader.tsx
@@ -1,34 +1,121 @@
-import { Group, Header, ActionIcon, useMantineColorScheme } from "@mantine/core";
-import { IconSun, IconMoonStars, IconHome2, IconBrandGithub } from "@tabler/icons";
-import ActiveLink from "./ActiveLink";
-import SignInButton from "@components/Auth/SignInButton";
-import Link from "next/link";
+import { useState } from 'react';
+import { createStyles, Header, Container, Group, Burger, Paper, Transition } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-const CustomHeader = () => {
-    const { colorScheme, toggleColorScheme} = useMantineColorScheme();
+const HEADER_HEIGHT = 60;
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+    zIndex: 1,
+  },
+
+  dropdown: {
+    position: 'absolute',
+    top: HEADER_HEIGHT,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: 'hidden',
+
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: '8px 12px',
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+
+    [theme.fn.smallerThan('sm')]: {
+      borderRadius: 0,
+      padding: theme.spacing.md,
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+    },
+  },
+}));
+
+interface CustomHeaderProps {
+    links: { link: string; label: string }[];
+  }
+
+  export function CustomHeader({ links }: CustomHeaderProps) {
+    const [opened, { toggle, close }] = useDisclosure(false);
+    const [active, setActive] = useState(links[0].link);
+    const { classes, cx } = useStyles();
+
+    const items = links.map((link) => (
+      <a
+        key={link.label}
+        href={link.link}
+        className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+        onClick={(event) => {
+          event.preventDefault();
+          setActive(link.link);
+          close();
+        }}
+      >
+        {link.label}
+      </a>
+    ));
+
     return (
-        <Header height={60} fixed={false}>
-            <Group sx={{ height: '100%' }} px={20} position="apart" >
-                <ActionIcon variant="default" component={Link} href="/" size={'lg'}>
-                    <IconHome2 size={20}/>
-                </ActionIcon>
-                <Group sx={{ width: 400}} position="apart">
-                    <ActiveLink href='/resume' text='Resume'/>
-                    <ActiveLink href='/blog' text='Blog'/>
-                    <ActiveLink href='/projects' text='Projects'/>
-                </Group>
-                <Group>
-                    <ActionIcon variant="default" component="a" href="https://github.com/gerritt-luoma/personal-site">
-                        <IconBrandGithub size={20}/>
-                    </ActionIcon>
-                    <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={'lg'}>
-                        { colorScheme === 'light' ? <IconMoonStars size={20} /> : <IconSun size={20}/> }
-                    </ActionIcon>
-                    <SignInButton />
-                </Group>
-            </Group>
-        </Header>
-    )
+      <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+        <Container className={classes.header}>
+          <Group spacing={5} className={classes.links}>
+            {items}
+          </Group>
+
+          <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+
+          <Transition transition="pop-top-right" duration={200} mounted={opened}>
+            {(styles) => (
+              <Paper className={classes.dropdown} withBorder style={styles}>
+                {items}
+              </Paper>
+            )}
+          </Transition>
+        </Container>
+      </Header>
+    );
 }
 
 export default CustomHeader;
