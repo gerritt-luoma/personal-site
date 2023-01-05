3614b3d54f4607c043e08e78a0d6ae2e4a792e33
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-04 17:22:28 -0700

    Added in basic component

--- components/CustomHeader/CustomHeader.tsx
+++ components/CustomHeader/CustomHeader.tsx
@@ -1,121 +1,76 @@
 import { useState } from 'react';
-import { createStyles, Header, Container, Group, Burger, Paper, Transition } from '@mantine/core';
+import { createStyles, Header, Container, Group, Burger, Paper, Transition, Flex, Button, ActionIcon } from '@mantine/core';
+import { IconHome } from '@tabler/icons';
+import Link from 'next/link';
 import { useDisclosure } from '@mantine/hooks';
+import { ThemeContext } from '@emotion/react';
 
 const HEADER_HEIGHT = 60;
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dropdown: {
-    position: 'absolute',
-    top: HEADER_HEIGHT,
-    left: 0,
-    right: 0,
-    zIndex: 0,
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    borderTopWidth: 0,
-    overflow: 'hidden',
-
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '100%',
-  },
-
-  links: {
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  burger: {
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  link: {
-    display: 'block',
-    lineHeight: 1,
-    padding: '8px 12px',
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-    },
-
-    [theme.fn.smallerThan('sm')]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
-    },
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-    },
-  },
-}));
 
 interface CustomHeaderProps {
-    links: { link: string; label: string }[];
-  }
+    links: {
+        link: string;
+        label: string
+    }[];
+}
 
-  export function CustomHeader({ links }: CustomHeaderProps) {
+
+const CustomHeader = () => {
+    const links = [
+        {
+            link: '/resume',
+            label: 'Resume'
+        },
+        {
+            link: '/blog',
+            label: 'Blog'
+        },
+        {
+            link: '/projects',
+            label: 'Projects'
+        },
+    ];
     const [opened, { toggle, close }] = useDisclosure(false);
     const [active, setActive] = useState(links[0].link);
-    const { classes, cx } = useStyles();
-
     const items = links.map((link) => (
-      <a
-        key={link.label}
-        href={link.link}
-        className={cx(classes.link, { [classes.linkActive]: active === link.link })}
-        onClick={(event) => {
-          event.preventDefault();
-          setActive(link.link);
-          close();
-        }}
-      >
-        {link.label}
-      </a>
+        <Button
+            key={link.label}
+            component={Link}
+            href={link.link}
+            onClick={(event) => {
+                event.preventDefault();
+                setActive(link.link);
+                close();
+            }}
+            color={link.link == active ? 'pink.6' : ''}
+        >
+            {link.label}
+        </Button>
     ));
 
     return (
-      <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
-        <Container className={classes.header}>
-          <Group spacing={5} className={classes.links}>
-            {items}
-          </Group>
+        <Header height={HEADER_HEIGHT} mb={'sm'}>
+            <Flex direction={'row'} justify={'space-between'} align='center' h={'100%'}>
+                <ActionIcon size={'md'} m={'sm'}>
+                    <IconHome/>
+                </ActionIcon>
+                <Group spacing={5}>
+                    {items}
+                </Group>
 
-          <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+                <Burger opened={opened} onClick={toggle} m={'sm'}/>
 
-          <Transition transition="pop-top-right" duration={200} mounted={opened}>
-            {(styles) => (
-              <Paper className={classes.dropdown} withBorder style={styles}>
-                {items}
-              </Paper>
-            )}
-          </Transition>
-        </Container>
-      </Header>
+                <Transition transition="pop-top-right" duration={200} mounted={opened}>
+                    {(styles) => (
+                        <Paper style={styles} withBorder pos={'absolute'} top={HEADER_HEIGHT} left={0} right={0}>
+                            {items}
+                        </Paper>
+                    )}
+                </Transition>
+            </Flex>
+        </Header>
     );
 }
 
-export default CustomHeader;
+            export default CustomHeader;
