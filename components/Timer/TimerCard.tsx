7071f87c727ad01cd604f08172a918f404d182c5
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-11 21:55:16 -0700

    Added form to add timers

---
+++ components/Timer/TimerCard.tsx
@@ -0,0 +1,75 @@
+import { Card, Flex, Text, Divider } from "@mantine/core";
+import { convertSecondsToString } from "@lib/timer/timerUtils";
+import ChangeTime from "./ChangeTime";
+import PlayPauseButton from "./PlayPauseButton";
+import { useState } from "react";
+
+export interface TimerCardProps {
+    title: string,
+    totalSeconds: number
+}
+
+const TimerCard = ({ title, totalSeconds }: TimerCardProps) => {
+    const [remainingSeconds, setRemainingSeconds] = useState(totalSeconds);
+    const [intervalID, setIntervalID] = useState(0);
+    const [running, setRunning] = useState(false);
+
+    const startTimer = () => {
+        if(!intervalID) {
+            setIntervalID(window.setInterval(tickTime, 1000));
+            setRunning(true);
+        }
+    }
+
+    const stopTimer = () => {
+        if(intervalID) {
+            window.clearInterval(intervalID);
+            setIntervalID(0);
+            setRunning(false);
+        }
+    }
+
+    const tickTime = () => {
+        if(0 < remainingSeconds) {
+            setRemainingSeconds(remainingSeconds => remainingSeconds - 1);
+        } else {
+            stopTimer();
+        }
+    }
+
+    const reduceTime = () => {
+        if(0 > (remainingSeconds - 300)) {
+            setRemainingSeconds(0);
+        } else {
+            setRemainingSeconds(remainingSeconds => remainingSeconds - 300);
+        }
+    }
+    const addTime = () => {
+        setRemainingSeconds(remainingSeconds => remainingSeconds + 300);
+    }
+
+    return (
+        <Card radius={'md'} withBorder w={300} shadow='lg' my={'md'}>
+            <Card.Section>
+                <Text size={50} weight='bold' ta='center' mt='sm' mx='sm'>
+                    {convertSecondsToString(remainingSeconds)}
+                </Text>
+            </Card.Section>
+            <Divider/>
+            <Card.Section>
+                <Text ta='center' weight={'normal'}>
+                    {title}
+                </Text>
+            </Card.Section>
+            <Card.Section>
+                <Flex justify={'space-evenly'} mx='md' align='center' m='sm'>
+                    <ChangeTime text={"-5"} onClick={reduceTime}/>
+                    <PlayPauseButton running={running} onClick={running ? stopTimer : startTimer}/>
+                    <ChangeTime text={"+5"} onClick={addTime}/>
+                </Flex>
+            </Card.Section>
+        </Card>
+    );
+}
+
+export default TimerCard;
--- pages/projects/timer/index.tsx
+++ pages/projects/timer/index.tsx
@@ -1,70 +1,59 @@
+import { Flex, TextInput, Button, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { useState } from "react";
-import { Text, Flex, Card, Divider } from "@mantine/core";
-import { convertSecondsToString } from "@lib/timer/timerUtils";
-import PlayPauseButton from "@components/Timer/PlayPauseButton";
-import ChangeTime from "@components/Timer/ChangeTime";
+import TimerCard, { TimerCardProps } from "@components/Timer/TimerCard";
 
 const Timer = () => {
-    const [remainingSeconds, setRemainingSeconds] = useState(0);
-    const [intervalID, setIntervalID] = useState(0);
-    const [running, setRunning] = useState(false);
+    const [items, setItems] = useState<TimerCardProps[]>([]);
 
-    const startTimer = () => {
-        if(!intervalID) {
-            setIntervalID(window.setInterval(tickTime, 1000));
-            setRunning(true);
-        }
-    }
+    const form = useForm({
+        initialValues: {
+            title: '',
+            numMinutes: '',
+        },
 
-    const stopTimer = () => {
-        if(intervalID) {
-            window.clearInterval(intervalID);
-            setIntervalID(0);
-            setRunning(false);
-        }
-    }
+        validate: {
+            title: (value) => (
+                (value.length < 30) && (value.length > 0) ? null : 'Titles must be less than 30 characters'
+            ),
+        },
 
-    const tickTime = () => {
-        if(0 < remainingSeconds) {
-            setRemainingSeconds(remainingSeconds => remainingSeconds - 1);
-        } else {
-            stopTimer();
-        }
-    }
-
-    const reduceTime = () => {
-        if(0 > (remainingSeconds - 300)) {
-            setRemainingSeconds(0);
-        } else {
-            setRemainingSeconds(remainingSeconds => remainingSeconds - 300);
-        }
-    }
-    const addTime = () => {
-        setRemainingSeconds(remainingSeconds => remainingSeconds + 300);
-    }
-
+        transformValues: (values) => ({
+            totalSeconds: Number(values.numMinutes)*60 || 0,
+            title: values.title || '',
+        }),
+    })
     return (
-        <Flex justify={'center'}>
-            <Card radius={'md'} withBorder w={300} shadow='lg'>
-                <Card.Section>
-                    <Text size={50} weight='bold' ta='center' mt='sm' mx='sm'>
-                        {convertSecondsToString(remainingSeconds)}
-                    </Text>
-                </Card.Section>
-                <Divider/>
-                <Card.Section>
-                    <Text ta='center' weight={'normal'}>
-                        Work on thing
-                    </Text>
-                </Card.Section>
-                <Card.Section>
-                    <Flex justify={'space-evenly'} mx='md' align='center' m='sm'>
-                        <ChangeTime text={"-5"} onClick={reduceTime}/>
-                        <PlayPauseButton running={running} onClick={running ? stopTimer : startTimer}/>
-                        <ChangeTime text={"+5"} onClick={addTime}/>
-                    </Flex>
-                </Card.Section>
-            </Card>
+        <Flex justify={'center'} align='center' direction='column' mx='auto' bg='dark.7' maw={500}>
+            <Box  bg='dark.6' w={300} mx='auto' sx={{borderRadius:'20px'}}>
+                <form
+                    onSubmit={form.onSubmit((values) => {
+                        const newArr = [...items, values]
+                        setItems(newArr);
+                        form.reset();
+                    })}
+                    style={{display: 'inline'}}
+                >
+                    <TextInput
+                        label='Title'
+                        placeholder="Title of task..."
+                        m='sm'
+                        miw={250}
+                        {...form.getInputProps('title')}
+                    />
+                    <TextInput
+                        label='Length'
+                        placeholder="Length in minutes"
+                        type={'number'}
+                        m='sm'
+                        {...form.getInputProps('numMinutes')}
+                    />
+                    <Button type='submit' m='sm'>
+                        Add
+                    </Button>
+                </form>
+            </Box>
+            {items && items.map((item, index) => <TimerCard key={index} title={item.title} totalSeconds={item.totalSeconds}/>)}
         </Flex>
     );
 }
