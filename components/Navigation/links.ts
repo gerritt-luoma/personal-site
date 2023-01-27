01f0789b76f94e71787f534e0937004ac914beab
gerritt-luoma <gerritt.luoma@gmail.com>
2023-01-27 14:59:18 -0700

    Added header links

---
+++ components/Navigation/links.ts
@@ -0,0 +1,15 @@
+import { TablerIcon, IconHome, IconArticle, IconBulb, IconBrandGithub } from "@tabler/icons";
+
+export interface LinkProps {
+    icon?: TablerIcon,
+    color?: string,
+    link: string,
+    label: string
+}
+
+export const navLinks: LinkProps[] = [
+    { icon: IconHome, color: 'pink', link: '/', label: 'Home' },
+    { icon: IconArticle, color: 'pink', link: '/blog', label: 'Blog' },
+    { icon: IconBulb, color: 'pink', link: '/projects', label: 'Projects' },
+    { icon: IconBrandGithub, color: 'pink', link: 'https://github.com/gerritt-luoma/personal-site', label: 'Github'}
+]
